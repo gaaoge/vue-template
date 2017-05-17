e8df226be7e9d3c446377d754fa42bffc38decd8
GG <723687948@qq.com>
2017-05-17 15:12:10 +0800

    修正客户端工具方法错误。

--- template/src/libs/newsapp-client.js
+++ template/src/libs/newsapp-client.js
@@ -433,7 +433,7 @@
             path = path || '/startup';
             channel = channel || 'sps';
 
-            if (isNewsApp()) {
+            if (isNewsApp) {
                 protocol('newsapp:/' + path);
             } else {
                 window.location.href = 'http://m.163.com/newsapp/applinks.html?path=' + encodeURIComponent(path) + '&s=' + channel;
