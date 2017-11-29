9ae4f8cbfd74d8fba9a68990b5ad941f0759384b
GG <723687948@qq.com>
2017-11-29 10:51:57 +0800

    优化newsapp-share工具类。

--- template/src/App.vue
+++ template/src/App.vue
@@ -21,7 +21,7 @@
       NewsappShare.update({
         title: '分享标题',
         desc: '分享描述',
-        img_url: NewsappShare.getAbsPath('resource/statics/share-icon.png')
+        img_url: 'resource/statics/share-icon.png'
       })
       NewsappShare.updateShareLink()
     }
--- template/src/utils/newsapp-share.js
+++ template/src/utils/newsapp-share.js
@@ -7,19 +7,23 @@
       img_url: '',
       link: ''
     },
-    update: function (data, onlyImg) {
+    update: function (data, imgMode) {
       for (var i in data) {
         if (this.shareData.hasOwnProperty(i)) {
           this.shareData[i] = data[i]
         }
+        if (i === 'img_url') {
+          this.shareData[i] = window.NewsappShare.getAbsPath(data[i])
+        }
       }
+
       var html = ''
-      html += '<div id="__newsapp_sharetext">' + (this.shareData.weibo || (this.shareData.title + ' ' + this.shareData.link)) + '</div>'
+      html += '<div id="__newsapp_sharetext">' + (this.shareData.weibo || this.shareData.title) + ' ' + this.shareData.link + '</div>'
       html += '<div id="__newsapp_sharephotourl">' + this.shareData.img_url + '</div>'
       html += '<div id="__newsapp_sharewxtitle">' + this.shareData.title + '</div>'
       html += '<div id="__newsapp_sharewxtext">' + this.shareData.desc + '</div>'
       html += '<div id="__newsapp_sharewxthumburl">' + this.shareData.img_url + '</div>'
-      if (!onlyImg) {
+      if (!imgMode) {
         html += '<div id="__newsapp_sharewxurl">' + this.shareData.link + '</div>'
       }
 
@@ -34,18 +38,18 @@
         return window.location.href.replace(/(\?|#).*/, '')
       }
     },
-    updateShareLink () {
+    updateShareLink (url) {
       if (window.NTESAntAnalysis) {
         window.NewsappShare.update({
-          link: window.NTESAntAnalysis.getShareLink(window.NewsappShare.getAbsPath())
+          link: window.NTESAntAnalysis.getShareLink(window.NewsappShare.getAbsPath(url))
         })
       } else {
         window.NewsappShare.update({
-          link: window.NewsappShare.getAbsPath()
+          link: window.NewsappShare.getAbsPath(url)
         })
         window.addEventListener('NTMReady', () => {
           window.NewsappShare.update({
-            link: window.NTESAntAnalysis.getShareLink(window.NewsappShare.getAbsPath())
+            link: window.NTESAntAnalysis.getShareLink(window.NewsappShare.getAbsPath(url))
           })
         })
       }
