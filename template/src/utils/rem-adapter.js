8057468b8e60fe750dcf444252778c74b6ab0c99
gaaoge <723687948@qq.com>
2018-01-09 13:45:56 +0800

    优化rem适配工具类，更新依赖库版本。

--- template/src/utils/rem-adapter.js
+++ template/src/utils/rem-adapter.js
@@ -1,42 +1,43 @@
-;(function () {
-  var designWidth = 750 // 设计稿宽度
-  var designHeight = 1206 // 设计稿高度
-  var designRem = 100 // 目标rem对应px的比例
-  var defaultRem = getDefaultRem() // 浏览器默认rem对应px的比例
-  var tid
+const designWidth = 750 // 设计稿宽度
+const designHeight = 1206 // 设计稿高度
+const designRem = 100 // 目标rem对应px的比例
+const defaultRem = getRem() // 浏览器默认rem对应px的比例
 
-  function getDefaultRem () {
-    var div = document.createElement('div')
-    div.style.width = '1rem'
-    var tempContainer = document.body || document.getElementsByTagName('head')[0]
-    tempContainer.appendChild(div)
-    var defaultRem = parseFloat(window.getComputedStyle(div, null).getPropertyValue('width'))
-    tempContainer.removeChild(div)
-    return defaultRem
+function getRem () {
+  let div = document.createElement('div')
+  div.style.width = '1rem'
+  let tempContainer = document.body || document.getElementsByTagName('head')[0]
+  tempContainer.appendChild(div)
+  let rem = parseFloat(window.getComputedStyle(div, null).getPropertyValue('width'))
+  tempContainer.removeChild(div)
+  return rem
+}
+
+function updateRem () {
+  let clientWidth
+  if (window.innerWidth < window.innerHeight) {
+    clientWidth = window.innerWidth
+  } else if (window.orientation === 0 || window.orientation === 180) {
+    clientWidth = window.innerWidth
+  } else {
+    clientWidth = designWidth / designHeight * window.innerHeight
   }
+  document.documentElement.style.fontSize = clientWidth / designWidth * designRem / defaultRem * 100 + '%'
+}
 
-  function updateRem () {
-    var clientWidth
-    if (window.innerWidth < window.innerHeight) {
-      clientWidth = window.innerWidth
-    } else if (window.orientation === 0 || window.orientation === 180) {
-      clientWidth = window.innerWidth
-    } else {
-      clientWidth = designWidth / designHeight * window.innerHeight
-    }
-    document.documentElement.style.fontSize = clientWidth / designWidth * designRem / defaultRem * 100 + '%'
-  }
-
-  window.addEventListener('resize', function () {
+let tid
+window.addEventListener('resize', function () {
+  clearTimeout(tid)
+  tid = setTimeout(updateRem, 100)
+}, false)
+window.addEventListener('pageshow', function (e) {
+  if (e.persisted) {
     clearTimeout(tid)
     tid = setTimeout(updateRem, 100)
-  }, false)
-  window.addEventListener('pageshow', function (e) {
-    if (e.persisted) {
-      clearTimeout(tid)
-      tid = setTimeout(updateRem, 100)
-    }
-  }, false)
+  }
+}, false)
+updateRem()
 
-  updateRem()
-}())
+export {
+  getRem
+}
