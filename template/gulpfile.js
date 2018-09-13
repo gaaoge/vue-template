ccaedd5066080ac9023dafb58f484a812b82a16f
gaaoge <723687948@qq.com>
2018-09-13 17:59:54 +0800

    优化调整项目发布。

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -7,8 +7,6 @@ const fs = require('fs')
 const path = require('path')
 const gulp = require('gulp')
 const exec = require('child_process').exec
-const replace = require('gulp-replace')
-const vinylftp = require('vinyl-ftp')
 const easeftp = require('easeftp/upload')
 const ftppass = JSON.parse(fs.readFileSync('.ftppass', 'utf-8'))
 
@@ -31,22 +29,17 @@ gulp.task('pre', function () {
 })
 
 gulp.task('publish', function () {
-  const statistics = [
-    '<script>var _ntes_ant_projectid="'+ pkg.projectid +'";(function(w,d,s,n) {var f=d.getElementsByTagName(s)[0],k=d.createElement(s);k.async=true;k.src="//static.ws.126.net/utf8/3g/analytics/data1/"+n+".js";f.parentNode.insertBefore(k,f);})(window,document,"script",_ntes_ant_projectid);</script>',
-    '<script>var _hmt=_hmt||[];(function(){var b=document.createElement("script");b.src="//hm.baidu.com/hm.js?7fa45cfaddbf8ba5591da1950285d665";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a)})();</script>'
-  ].join('')
-
-  const conn = vinylftp.create(ftppass.vinylftp)
-  gulp.src(['dist/index.html', 'dist/service-worker.js'])
-    .pipe(replace('<!--statistics-->', statistics))
-    .pipe(conn.dest('qa/activity/' + pkg.name))
+  easeftp.addFile(['index.html', 'service-worker.js'], {
+    debug: true,
+    ...ftppass.easeftp,
+    path: 'html/activity/' + pkg.name,
+    cwd: path.resolve('dist')
+  })
 
   return easeftp.addFile(['**/*'], {
     debug: true,
     ...ftppass.easeftp,
     path: 'activity/' + pkg.name + '/static',
     cwd: path.resolve('dist/static')
-  }).then((data) => {
-    console.log(data.urls)
   })
 })
