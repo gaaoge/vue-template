c7017d47481060174377c11a716474250f36ce7a
gaaoge <723687948@qq.com>
2018-11-12 11:29:33 +0800

    调整移动端console, 添加错误监控。

--- template/src/main.js
+++ template/src/main.js
@@ -12,7 +12,7 @@ import router from './router'
 import store from './store'
 import App from './App'
 
-import { loadScript } from '@/utils'
+import { loadScript, getSearch } from '@/utils'
 import { trackEvent, projectId } from '@/utils/track'
 
 // 注册全局指令
@@ -33,22 +33,25 @@ window.vm = new Vue({
 })
 
 // 移动端console
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' || getSearch('debug')) {
   loadScript('//cdn.jsdelivr.net/npm/eruda', () => {
     window.eruda.init()
   })
 }
 
-// 离线缓存Service Worker
 if (process.env.NODE_ENV === 'production') {
+  // 离线缓存Service Worker
   if ('serviceWorker' in navigator && window.location.protocol === 'https:') {
     window.onload = () => {
       navigator.serviceWorker.register('service-worker.js')
     }
   }
-}
 
-// 统计
-if (process.env.NODE_ENV === 'production') {
+  // 统计
   loadScript('//static.ws.126.net/163/frontend/antnest/' + projectId + '.js')
+
+  // 错误监控
+  loadScript('https://static.ws.126.net/163/frontend/libs/raven-vue-3.26.4.min.js', () => {
+    window.Raven.config('https://76981b9dcb584c2990e70dc596eeac3c@sentry.music.163.com/13').install()
+  })
 }
