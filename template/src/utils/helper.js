58b77cc6d5dca90fc014c4528d8843018caaa94a
gaaoge <723687948@qq.com>
2018-11-17 20:37:44 +0800

    添加错误监控tags配置。

--- template/src/utils/helper.js
+++ template/src/utils/helper.js
@@ -15,7 +15,11 @@ if (process.env.NODE_ENV === 'production') {
 
   // 错误监控
   loadScript('https://static.ws.126.net/163/frontend/libs/raven-vue-3.26.4.min.js', () => {
-    window.Raven.config('https://76981b9dcb584c2990e70dc596eeac3c@sentry.music.163.com/13').install()
+    window.Raven.config('https://76981b9dcb584c2990e70dc596eeac3c@sentry.music.163.com/13', {
+      tags: {
+        project: document.title
+      }
+    }).install()
   })
 
   // 离线缓存Service Worker
