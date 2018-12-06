6e011c78ec43425d64dca91f49b3b5248231108b
gaaoge <723687948@qq.com>
2018-12-06 16:40:56 +0800

    优化单次自定义方法。

--- template/src/utils/share.js
+++ template/src/utils/share.js
@@ -4,7 +4,7 @@ import NewsappShare from 'newsapp-share'
 import NewsappAPI from 'newsapp-api'
 
 window.NewsappAPI = NewsappAPI
-const shareConfig = {
+const defaultConfig = {
   title: '默认分享标题',
   desc: '默认分享描述',
   imgUrl: getStaticPath('share-icon.png'),
@@ -15,27 +15,32 @@ const shareConfig = {
     trackEvent('sharedone')
   }
 }
+let customConfig = {}
 
 function updateShareConfig (config = {}) {
   const origin = config.shareDone
   config.shareDone = () => {
     origin && origin.apply(this, arguments)
-    shareConfig.shareDone.apply(this, arguments)
+    defaultConfig.shareDone.apply(this, arguments)
   }
 
-  NewsappShare.config(Object.assign({}, shareConfig, config))
+  customConfig = Object.assign({}, defaultConfig, config)
+  NewsappShare.config(customConfig)
 }
 
 function shareWithConfig (config = {}, tag) {
   const origin = config.shareDone
   config.shareDone = () => {
     origin && origin.apply(this, arguments)
-    updateShareConfig()
+    defaultConfig.shareDone.apply(this, arguments)
+    updateShareConfig(customConfig)
   }
 
-  updateShareConfig(config)
+  NewsappShare.config(config)
   NewsappShare.show(tag)
-  tag && setTimeout(updateShareConfig, 300)
+  tag && setTimeout(() => {
+    updateShareConfig(customConfig)
+  }, 300)
 }
 
 export {
