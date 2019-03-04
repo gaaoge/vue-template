28585ef7b5868d5f3bdad61626563ff67753f894
gaaoge <gaaoge@163.com>
2019-03-04 18:08:55 +0800

    优化加载js的请求头兼容。

--- template/src/utils/helper.js
+++ template/src/utils/helper.js
@@ -10,12 +10,12 @@ if (process.env.NODE_ENV === 'development' || /debug/ig.test(location.hash)) {
 
 if (process.env.NODE_ENV === 'production') {
   // 统计
-  loadScript('https://static.ws.126.net/163/frontend/libs/antanalysis.min.js')
-  loadScript('https://static.ws.126.net/163/frontend/antnest/' + process.env.VUE_APP_PROJECT_ID + '.js')
-  loadScript('https://hm.baidu.com/hm.js?fbbd5a62f1db722ba672bc37a9bf6b05')
+  loadScript('//static.ws.126.net/163/frontend/libs/antanalysis.min.js')
+  loadScript('//static.ws.126.net/163/frontend/antnest/' + process.env.VUE_APP_PROJECT_ID + '.js')
+  loadScript('//hm.baidu.com/hm.js?fbbd5a62f1db722ba672bc37a9bf6b05')
 
   // 错误监控
-  loadScript('https://static.ws.126.net/163/frontend/libs/raven-vue-3.26.4.min.js', () => {
+  loadScript('//static.ws.126.net/163/frontend/libs/raven-vue-3.26.4.min.js', () => {
     window.Raven.config('https://76981b9dcb584c2990e70dc596eeac3c@sentry.music.163.com/13', {
       tags: {
         project: process.env.VUE_APP_TITLE
