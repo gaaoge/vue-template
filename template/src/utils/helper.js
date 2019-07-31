fe7e9ec8c86d08e988431e683ebaee21b63cccfb
gaaoge <gaaoge@163.com>
2019-07-31 16:31:23 +0800

    优化统计及错误监控，更新依赖库。

--- template/src/utils/helper.js
+++ template/src/utils/helper.js
@@ -9,30 +9,8 @@ if (process.env.NODE_ENV === 'development' || /debug/gi.test(location.hash)) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  // 统计
-  loadScript('//static.ws.126.net/163/frontend/libs/antanalysis.min.js')
-  loadScript(
-    '//static.ws.126.net/163/frontend/antnest/' +
-      process.env.VUE_APP_PROJECT_ID +
-      '.js'
-  )
-  loadScript('//hm.baidu.com/hm.js?fbbd5a62f1db722ba672bc37a9bf6b05')
-
-  // 错误监控
-  loadScript(
-    '//static.ws.126.net/163/frontend/libs/raven-vue-3.26.4.min.js',
-    () => {
-      window.Raven.config(
-        'https://76981b9dcb584c2990e70dc596eeac3c@sentry.music.163.com/13',
-        {
-          tags: {
-            project: process.env.VUE_APP_TITLE,
-            projectId: process.env.VUE_APP_PROJECT_ID
-          }
-        }
-      ).install()
-    }
-  )
+  // 统计及错误监控
+  loadScript('//static.ws.126.net/163/frontend/antnest/NTM-BXR8M5Z5-1.js')
 
   // 离线缓存Service Worker
   register('service-worker.js', {
