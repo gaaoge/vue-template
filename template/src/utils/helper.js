c8fe4fdd183cfa0ec51010ca34feb7309a62f530
gaaoge <gaaoge@163.com>
2019-07-31 16:46:44 +0800

    优化统计相关。

--- template/src/utils/helper.js
+++ template/src/utils/helper.js
@@ -1,4 +1,5 @@
 import { loadScript } from '@/utils/index'
+import { trackEvent } from '@/utils/track'
 import { register } from 'register-service-worker'
 
 // 移动端console
@@ -9,7 +10,15 @@ if (process.env.NODE_ENV === 'development' || /debug/gi.test(location.hash)) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  // 统计及错误监控
+  // 统计
+  loadScript(
+    'https://static.ws.126.net/163/frontend/libs/antanalysis.min.js',
+    () => {
+      trackEvent('pageview')
+    }
+  )
+
+  // 统一性能统计及错误监控
   loadScript('//static.ws.126.net/163/frontend/antnest/NTM-BXR8M5Z5-1.js')
 
   // 离线缓存Service Worker
