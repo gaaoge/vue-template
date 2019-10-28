511af7dc731bdbcea1fdde23f8286d1270dcfddf
gaaoge <gaaoge@163.com>
2019-10-28 18:59:25 +0800

    去除离线模板pwa配置。

---
+++ template/offline/vue.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const apiMocker = require('mocker-api')
+
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VUE_APP_STATIC_PATH
+      : '',
+  productionSourceMap: false,
+  assetsDir: 'static',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'js-bridge': '@mf2e/js-bridge',
+        'newsapp-share': '@newsapp-activity/newsapp-share'
+      }
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            name: 'vendors'
+          }
+        }
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g)(\?.*)?$/)
+      .use('tinify-loader')
+      .loader('tinify-loader')
+      .tap(() => {
+        return {
+          apikey: 'ai3NQ23wq2pbQvy2JNylfuQMNJ99YAOZ',
+          cache: path.resolve('node_modules/.cache/tinify-loader')
+        }
+      })
+
+    config.module.rule('svg').test(/\.(svg|gif|webp)(\?.*)?$/)
+  },
+  devServer: {
+    before(app) {
+      apiMocker(app, path.resolve('./mock/index.js'))
+    }
+  }
+}
