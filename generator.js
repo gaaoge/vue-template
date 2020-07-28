6f742944fafa6cb74577b4d5350bd1f3d948af92
gaaoge <gaaoge@163.com>
2020-07-28 16:19:49 +0800

    更新依赖库版本。

--- generator.js
+++ generator.js
@@ -12,40 +12,37 @@ module.exports = (api, options, rootOptions) => {
       },
       dependencies: {
         '@mf2e/js-bridge': '^1.0.11',
-        '@newsapp-activity/newsapp-share': '^1.6.5',
+        '@newsapp-activity/newsapp-share': '^1.6.7',
         'better-scroll': '^2.0.0-beta.6',
-        'core-js': '^3.6.5',
         'minireset.css': '^0.0.6',
         'register-service-worker': '^1.7.1',
         'vue': '^2.6.11',
-        'vue-router': '^3.1.6',
-        'vuex': '^3.1.3',
-        'whatwg-fetch': '^3.0.0',
+        'vue-router': '^3.3.4',
+        'vuex': '^3.5.1',
+        'whatwg-fetch': '^3.2.0',
       },
       devDependencies: {
         '@newap/uploader': '^2.2.16',
-        '@vue/cli-plugin-babel': '^4.3.1',
-        '@vue/cli-plugin-eslint': '^4.3.1',
-        '@vue/cli-plugin-pwa': '^4.3.1',
-        '@vue/cli-plugin-router': '^4.3.1',
-        '@vue/cli-plugin-vuex': '^4.3.1',
-        '@vue/cli-service': '^4.3.1',
+        '@vue/cli-plugin-babel': '^4.4.6',
+        '@vue/cli-plugin-eslint': '^4.4.6',
+        '@vue/cli-plugin-pwa': '^4.4.6',
+        '@vue/cli-service': '^4.4.6',
         '@vue/eslint-config-prettier': '^6.0.0',
         'babel-eslint': '^10.1.0',
-        'chalk': '^4.0.0',
-        'eslint': '^6.8.0',
-        'eslint-plugin-prettier': '^3.1.3',
+        'chalk': '^4.1.0',
+        'eslint': '^7.5.0',
+        'eslint-plugin-prettier': '^3.1.4',
         'eslint-plugin-vue': '^6.2.2',
-        'inquirer': '^7.1.0',
-        'mocker-api': '^2.0.3',
+        'inquirer': '^7.3.3',
+        'mocker-api': '^2.2.0',
         'postcss-autosize': '^1.0.2',
         'postcss-plugin-px2rem': '^0.8.1',
         'postcss-preset-env': '^6.7.0',
-        'prettier': '^2.0.4',
+        'prettier': '^2.0.5',
         'tinify-loader': '^1.0.0',
         'vue-template-compiler': '^2.6.11',
       },
-      browserslist: ['> 1%', 'last 2 versions', 'Android >= 4.4', 'iOS >= 8'],
+      browserslist: ['> 1%', 'last 2 versions', 'Android >= 4', 'iOS >= 8'],
     }
   })
 
@@ -61,7 +58,7 @@ module.exports = (api, options, rootOptions) => {
     api.extendPackage(() => {
       return {
         dependencies: {
-          'pixi.js-legacy': '^5.2.1',
+          'pixi.js-legacy': '^5.3.2',
           'pixi5-dragonbones': '^5.7.0',
         },
       }
@@ -75,7 +72,7 @@ module.exports = (api, options, rootOptions) => {
     api.extendPackage(() => {
       return {
         devDependencies: {
-          '@mf2e/offline-tool': '^0.1.0',
+          '@mf2e/offline-tool': '^0.1.1',
         },
       }
     })
